5fb8b98cb32bf7e689350dd2f31496ba42280125
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-02-29 22:36:37 -0800

    Working on the tests.

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,11 +1,10 @@
 const { Shape, Circle, Triangle, Square } = require("../lib/shapes");
 
 describe("Circle", () => {
-	describe("render", () => {
+	describe("shapeColor", () => {
 		it("should throw an error when render is called directly", () => {
-			// // arrange
 			const shape = new Triangle();
-			shape.setColor("blue");
+			this.shapeColor = "blue";
 			expect(shape.render()).toEqual(
 				'<polygon points="150, 18 244, 182 56, 182" fill="blue" />'
 			);
